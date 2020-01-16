2b2e58cdeb352b40a2f90f7febb741ea36488d2e
lixinxin <1733571444@qq.com>
2020-01-16 12:31:40 +0800

    1

--- spring-cloud-producer/src/main/java/com/example/springcloudproducer/controller/HelloController.java
+++ spring-cloud-producer/src/main/java/com/example/springcloudproducer/controller/HelloController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
         return "hello "+name+"，this is first messge";
